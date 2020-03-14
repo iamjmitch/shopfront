bb7459aea6668c9eaa78d133048b5b817989abfd
James Mitchell <iamjmitch1990@gmail.com>
2020-03-14 15:11:34 +1000

    header component added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
-import './App.css';
-import Logo from './assets/global/logo.svg';
-
 import HomePage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component';
+import './App.css';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
